b90fbd4863b6b9ba8b7fdfca1e8e910e9c0f1ec0
Andrew Hale <hale.adh@gmail.com>
2021-07-01 17:56:28 -0500

    firefox wrapper fix

--- figure-slider/assets/script.js
+++ figure-slider/assets/script.js
@@ -9,3 +9,16 @@ new Glider(document.querySelector('.glider'), {
 		next: '.slider-next',
 	},
 });
+
+document.addEventListener('glider-loaded', hideFFScrollBars);
+document.addEventListener('glider-refresh', hideFFScrollBars);
+function hideFFScrollBars(e) {
+	var scrollbarHeight = 17; // Currently 17, may change with updates
+	if (/firefox/i.test(navigator.userAgent)) {
+		// We only need to appy to desktop. Firefox for mobile uses
+		// a different rendering engine (WebKit)
+		if (window.innerWidth > 575) {
+			e.target.parentNode.style.height = e.target.offsetHeight - scrollbarHeight + 'px';
+		}
+	}
+}
