3be541a78cf6679d97f0e77650e21653b5ef569a
Andrew Hale <hale.adh@gmail.com>
2021-07-23 22:16:40 -0500

    push chilis

---
+++ chilli-list/assets/script.js
@@ -0,0 +1,45 @@
+const sliders = document.querySelectorAll('.slider');
+const gliders = [];
+sliders.forEach((slider) => {
+	let prev = slider.querySelector('.slider-prev');
+	let next = slider.querySelector('.slider-next');
+	let dots = slider.querySelector('.slider-dots');
+	let slides = slider.querySelector('.slides');
+
+	// required for accessibility
+	slider.setAttribute('role', 'tablist');
+
+	let glider = new Glider(slides, {
+		slidesToShow: 1,
+		dots: dots,
+		draggable: true,
+		scrollLock: true,
+		duration: 0.7,
+		arrows: {
+			prev: prev,
+			next: next,
+		},
+	});
+	gliders.push(glider);
+	setTimeout(function () {
+		glider.refresh();
+	}, 0);
+});
+const isFirefox = /firefox/i.test(navigator.userAgent);
+const scrollbarHeight = 17;
+
+document.addEventListener('glider-loaded', hideFFScrollBars);
+document.addEventListener('glider-refresh', hideFFScrollBars);
+function hideFFScrollBars(e) {
+	// Currently 17, may change with updates
+	if (isFirefox) {
+		console.log('loaded');
+		// We only need to appy to desktop. Firefox for mobile uses
+		// a different rendering engine (WebKit)
+		if (window.innerWidth > 575) {
+			e.target.parentNode.style.height = e.target.offsetHeight - scrollbarHeight + 'px';
+		} else {
+			e.target.parentNode.removeAttribute('style');
+		}
+	}
+}
